b1cd0a62176a777053792f5743b925c2bd21ea67
bsaglam <burcu.saglam001@gmail.com>
2020-11-30 05:47:02 +0300

    ViewDen=>Controller'a veri gönderimi.parametre ve Request.Form["elementName"] ile alma

---
+++ 28-ViewDenControlleraVeriGönderimi-/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _28_ViewDenControlleraVeriGönderimi_.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string name)
+        {
+            string getWithparameter = name;
+            string getRequestForm = Request.Form["name"];
+
+            
+            return View();
+        }
+    }
+}
---
+++ 28-ViewDenControlleraVeriGönderimi-/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_28_ViewDenControlleraVeriGönderimi_.MvcApplication" Language="C#" %>
