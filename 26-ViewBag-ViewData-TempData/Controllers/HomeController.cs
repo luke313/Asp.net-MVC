3026aafeba728599f01cd731cbfe23f34a86bda3
bsaglam <burcu.saglam001@gmail.com>
2020-11-30 05:05:35 +0300

    Controller=>View'e data aktarımı

---
+++ 26-ViewBag-ViewData-TempData/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _26_ViewBag_ViewData_TempData.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["text1"] = "ViewData ile gönderilen içerik";
+            ViewBag.text2 = "ViewBag ile gönderilen içerik";
+
+            TempData["text3"] = "Tempdata kullanımı";
+            return View();
+        }
+
+        public ActionResult TempDataForSecond()
+        {
+            return View();
+        }
+    }
+}
---
+++ 26-ViewBag-ViewData-TempData/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_26_ViewBag_ViewData_TempData.MvcApplication" Language="C#" %>
